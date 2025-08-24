cc72b8e8c16d64fe9adab396b7d68add29562894
Jayden <Jaydenbro8868@gmail.com>
2025-08-24 01:30:12 -0700

    Initialize Spring boot Maven backend for local gems

---
+++ Backend/src/main/java/com/localgems/localgems_backend/LocalGemsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.localgems.localgems_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalGemsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalGemsBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/localgems/localgems_backend/LocalGemsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localgems.localgems_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalGemsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
