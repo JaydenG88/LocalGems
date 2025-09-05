b7f0f675a1c2f0d1542c748c1d10046574c1640b
Jayden <Jaydenbro8868@gmail.com>
2025-09-04 20:19:55 -0700

    Build basic CRUD for City entity

--- Backend/src/main/java/com/localgems/localgems_backend/service/CityService.java
+++ Backend/src/main/java/com/localgems/localgems_backend/service/CityService.java
@@ -1,7 +1,58 @@
 package com.localgems.localgems_backend.service;
 import org.springframework.stereotype.Service;
 import com.localgems.localgems_backend.model.City;
+import com.localgems.localgems_backend.dto.CityRequestDTO;
+import com.localgems.localgems_backend.dto.CityResponseDTO;
+import com.localgems.localgems_backend.mapper.CityMapper;
+import com.localgems.localgems_backend.repository.CityRepository;
+import java.util.*;
+
 
 public class CityService {
-    
+    private final CityRepository cityRepository;
+    private final CityMapper cityMapper;
+
+    public CityService(CityRepository cityRepository, CityMapper cityMapper) {
+        this.cityRepository = cityRepository;
+        this.cityMapper = cityMapper;
+
+    }
+
+    public City createCity(CityRequestDTO cityRequestDTO) {
+        City city = cityMapper.dtoToEntity(cityRequestDTO);
+        City savedCity = cityRepository.save(city);
+        return savedCity;
+    }
+
+    public List<CityResponseDTO> getAllCities() {
+        List<City> cities = cityRepository.findAll();
+        List<CityResponseDTO> cityResponseDTOs = new ArrayList<>();
+        for (City city : cities) {
+            cityResponseDTOs.add(cityMapper.entityToDto(city));
+        }
+
+        return cityResponseDTOs;
+    }
+
+    public Optional<CityResponseDTO> getCityById(Long id) {
+        Optional<City> cityOpt = cityRepository.findById(id);
+        return cityOpt.map(city -> cityMapper.entityToDto(city) );
+    }
+
+    public CityResponseDTO updateCity(Long id, CityRequestDTO cityRequestDTO) {
+        City city = cityRepository.findById(id)
+            .orElseThrow(() -> new NoSuchElementException("Category not found with id: " + id));
+        cityMapper.updateEntityFromDto(cityRequestDTO, city);
+        City updatedCity = cityRepository.save(city);
+        return cityMapper.entityToDto(updatedCity);
+        
+    }
+
+    public void deleteCity(Long id) {
+        if (cityRepository.existsById(id)) {
+            cityRepository.deleteById(id);
+        } else {
+            throw new NoSuchElementException("City not found with id: " + id);
+        }
+    }
 }
