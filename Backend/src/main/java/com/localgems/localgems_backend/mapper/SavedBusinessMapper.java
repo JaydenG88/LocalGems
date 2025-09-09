aff62aa079c3598da006c2a358c83623ca97c16d
Jayden <Jaydenbro8868@gmail.com>
2025-09-09 16:14:28 -0700

    Fix to show user and business ids

--- Backend/src/main/java/com/localgems/localgems_backend/mapper/SavedBusinessMapper.java
+++ Backend/src/main/java/com/localgems/localgems_backend/mapper/SavedBusinessMapper.java
@@ -1,6 +1,7 @@
 package com.localgems.localgems_backend.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import com.localgems.localgems_backend.model.SavedBusiness;
 import com.localgems.localgems_backend.dto.requestDTO.SavedBusinessRequestDTO;
@@ -12,6 +13,8 @@ public interface SavedBusinessMapper {
     public static final SavedBusinessMapper INSTANCE = Mappers.getMapper(SavedBusinessMapper.class);
     SavedBusiness dtoToEntity(SavedBusinessRequestDTO dto);
 
+    @Mapping(source = "user.userId", target = "userId")
+    @Mapping(source = "business.businessId", target = "businessId")
     SavedBusinessResponseDTO entityToDto(SavedBusiness savedBusiness);
 
     void updateEntityFromDto(SavedBusinessRequestDTO dto, @MappingTarget SavedBusiness entity);
