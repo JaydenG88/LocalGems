af24bd65e0d59c03813c952be329a92991607674
Jayden <Jaydenbro8868@gmail.com>
2025-09-21 17:56:01 -0700

    add openAI key to app props and fix pom.xml build.

--- Backend/src/main/java/com/localgems/localgems_backend/mapper/BusinessMapper.java
+++ Backend/src/main/java/com/localgems/localgems_backend/mapper/BusinessMapper.java
@@ -1,4 +1,5 @@
 package com.localgems.localgems_backend.mapper;
+import com.localgems.localgems_backend.model.Category;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -7,6 +8,7 @@ import com.localgems.localgems_backend.model.Business;
 import com.localgems.localgems_backend.dto.requestDTO.BusinessRequestDTO;
 import com.localgems.localgems_backend.dto.responseDTO.BusinessResponseDTO;
 import com.localgems.localgems_backend.dto.responseDTO.CategoryResponseDTO;
+
 
 @Mapper(componentModel = "spring")
 public interface BusinessMapper {
@@ -18,7 +20,8 @@ public interface BusinessMapper {
     @Mapping(source = "city.name", target = "cityName")
     @Mapping(source = "city.cityId", target = "cityId")
     BusinessResponseDTO entityToDto(Business business);
-    CategoryResponseDTO categoryToDto(CategoryResponseDTO category);
+    
+    CategoryResponseDTO categoryToDto(Category category);
 
     void updateEntityFromDto(BusinessRequestDTO dto, @MappingTarget Business entity);
     
